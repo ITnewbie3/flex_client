2329e7ff1091254a8e97252d8a9fb7bdab9857d5
suhan <sd98221@naver.com>
2022-08-12 09:46:45 +0900

    오타수정

--- src/config/amuguna.js
+++ src/config/amuguna.js
@@ -1,3 +1,3 @@
 export const API_URL = "https://flexmovie-server.herokuapp.com"
 
-export const API_URL2 = "https://flexserver-chat.herokuapp.com/"
+export const API_URL2 = "https://flexserver-chat.herokuapp.com"
